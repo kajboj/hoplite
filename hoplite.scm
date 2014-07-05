1da179fceaa80a8665780286b0518c24fd97237c
Kajetan Bojko <kai@shutl.co.uk>
2014-07-05 11:39:02 +0100

    on-board predicate and neighbours function

--- hoplite.scm
+++ hoplite.scm
@@ -11,11 +11,31 @@
 (define (piece symbol coords)
   (list symbol coords))
 
-(define player (piece " P " '(5 -2)))
+(define (on-board? coords)
+  (let ((x (get-x coords)) (y (get-y coords)))
+    (and
+      (>= x 0)
+      (<= y 4)
+      (<= (+ x y) 10)
+      (<= x 10)
+      (>= y -4)
+      (>= (+ x y) 0))))
+
+(define (coords-add coords1 coords2)
+  (map + coords1 coords2))
+
+(define (neighbours coords)
+  (map
+    (lambda (x) (coords-add coords x))
+    (list '(-1 0) '(-1 1) '(0 1) '(1 0) '(1 -1) '(0 -1))))
+
+(define player (piece " P " '(5 0)))
 (define enemy (piece " E " '(10 0)))
 
 (displayn 
   (render-piece
     player
-    (render-piece enemy empty-board board-with-coords)
+    (render-piece enemy board-with-coords board-with-coords)
     board-with-coords))
+
+(displayn (neighbours (get-coords player)))
