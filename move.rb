2e423f0a4e14ba97c95c1a51bb867a8ce610ea02
Kajetan Bojko <kai@shutl.co.uk>
2014-07-08 07:53:46 +0100

    move touches the fixed point on the board

--- move.rb
+++ move.rb
@@ -2,4 +2,9 @@ adb = "/home/kajboj/code/adt/adt-bundle-linux-x86-20131030/sdk/platform-tools/ad
 regex = 's/\x0D\x0A/\x0A/g'
 `#{adb} shell screencap -p | perl -pe '#{regex}' > screen.png`
 load 'png_reader.rb'
-puts `scheme --silent < hoplite.scm`
+output = `scheme --silent < hoplite.scm`
+puts output
+
+col, row = 600, 550
+
+`./adb shell input tap #{col} #{row}`
