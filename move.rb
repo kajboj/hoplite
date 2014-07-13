01f2b2c0596b5f1aa7a868b1f13f78567d003dc3
Kajetan Bojko <kai@shutl.co.uk>
2014-07-13 19:44:17 +0100

    looping can be controlled from command line

--- move.rb
+++ move.rb
@@ -1,7 +1,17 @@
 adb = "/home/kajboj/code/adt/adt-bundle-linux-x86-20131030/sdk/platform-tools/adb"
 regex = 's/\x0D\x0A/\x0A/g'
 
-while true do
+def runner(repeat)
+  if repeat
+    while true do
+      yield
+    end
+  else
+    yield
+  end
+end
+
+runner(ENV['LOOP']) do
   puts 'getting image from android'
   `#{adb} shell screencap -p | perl -pe '#{regex}' > png/screen.png`
   puts 'processing image'
